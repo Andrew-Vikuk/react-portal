065665a8513828ef0a2bd9ce19173e1cb78380f6
Andrew-Vikuk <andriy.vikuk@gmail.com>
2024-05-29 15:58:04 +0300

    main

---
+++ src/scripts/CoursesListJs.js
@@ -0,0 +1,82 @@
+let courseListJs = [
+  {
+    title: "Що таке JavaScript",
+    des: "JavaScript - це високорівнева, інтерпретована мова програмування, яка використовується для створення інтерактивних елементів на веб-сторінках.",
+    img: "assets/img/about/js-icon_1.png",
+    link: "/js-start",
+  },
+  {
+    title: "Налаштування середовища",
+    des: "Перш ніж почати розробку з JavaScript або React, важливо налаштувати відповідне середовище для продуктивної роботи. Це включає встановлення Node.js та npm, вибір редактора коду та основи роботи з консоллю.",
+    img: "assets/img/about/js-icon_1.png",
+    link: "/js-settings",
+  },
+  {
+    title: "Синтаксис та основні оператори",
+    des: "Щоб ефективно працювати з JavaScript, важливо розуміти основи синтаксису та основні оператори.",
+    img: "assets/img/about/js-icon_1.png",
+    link: "/js-syntax",
+  },
+    {
+    title: "Функції",
+    des: " Функції - це один з основних будівельних блоків в JavaScript. Вони дозволяють групувати код у багаторазово використовувані блоки.",
+    img: "assets/img/about/js-icon_1.png",
+    link: "/js-function",
+  },
+    {
+    title: "Об'єкти та масиви",
+    des: "Об'єкти та масиви є фундаментальними структурами даних в JavaScript, що дозволяють зберігати та маніпулювати складними даними.",
+    img: "assets/img/about/js-icon_1.png",
+    link: "/js-objects",
+  },
+    {
+    title: "Умовні оператори та цикли",
+    des: "Lorem is Ipsum is simply is design iomyi is text Lorem Ipsum is simply is our busi Lorem is Ipsum is",
+    img: "assets/img/about/js-icon_1.png",
+    link: "/js-cycle",
+  },
+    {
+    title: "Замикання (Closures)",
+    des: "Lorem is Ipsum is simply is design iomyi is text Lorem Ipsum is simply is our busi Lorem is Ipsum is",
+    img: "assets/img/about/js-icon_1.png",
+    link: "/js-closures",
+  },
+    {
+    title: "Контекст виклику (this)",
+    des: "Lorem is Ipsum is simply is design iomyi is text Lorem Ipsum is simply is our busi Lorem is Ipsum is",
+    img: "assets/img/about/js-icon_1.png",
+    link: "/js-contex",
+  },
+    {
+    title: "Прототипне наслідування",
+    des: "Lorem is Ipsum is simply is design iomyi is text Lorem Ipsum is simply is our busi Lorem is Ipsum is",
+    img: "assets/img/about/js-icon_1.png",
+    link: "/js-imitation",
+  },
+    {
+    title: "Класи",
+    des: "Lorem is Ipsum is simply is design iomyi is text Lorem Ipsum is simply is our busi Lorem is Ipsum is",
+    img: "assets/img/about/js-icon_1.png",
+    link: "/js-classes",
+  },
+    {
+    title: "Колбеки",
+    des: "Lorem is Ipsum is simply is design iomyi is text Lorem Ipsum is simply is our busi Lorem is Ipsum is",
+    img: "assets/img/about/js-icon_1.png",
+    link: "/js-callback",
+  },
+    {
+    title: "Проміси",
+    des: "Lorem is Ipsum is simply is design iomyi is text Lorem Ipsum is simply is our busi Lorem is Ipsum is",
+    img: "assets/img/about/js-icon_1.png",
+    link: "/js-promise",
+  },
+    {
+    title: "Async/Await",
+    des: "Lorem is Ipsum is simply is design iomyi is text Lorem Ipsum is simply is our busi Lorem is Ipsum is",
+    img: "assets/img/about/js-icon_1.png",
+    link: "/js-async",
+  },
+];
+
+export default courseListJs;
